ddb22a13db751385fa338dc2cde8176be514f3e7
Дмитрий Солодовников <jokerbladeofstorme@gmail.com>
2020-01-13 06:43:14 +0300

    Work in progress. Mimicked.

--- web/resources/js/mainScript.js
+++ web/resources/js/mainScript.js
@@ -5,13 +5,46 @@ window.rInput = document.getElementById("r-visible");
 window.axes = document.getElementById("axes");
 
 function start() {
-    draw();
     axes.addEventListener("click", submitClickCanvas);
-}
 
-function draw() {
     const context = axes.getContext("2d");
     const r = document.getElementById("r-visible").value;
+    const c = 2/3;
+    context.clearRect(0, 0, axes.width, axes.height);
+
+    //circle
+    context.translate(200, 200);
+    context.beginPath();
+    context.arc(0, 0, 130*c, 3*Math.PI/2, 2*Math.PI, false);
+    context.closePath();
+    context.strokeStyle = "#2f9aff";
+    context.fillStyle = "#2f9aff";
+    context.fill();
+    context.stroke();
+
+    //triangle
+    context.translate(200, 200);
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(-260*c, 0);
+    context.lineTo(0, 260*c);
+    context.lineTo(0, 0);
+    context.closePath();
+    context.strokeStyle = "#2f9aff";
+    context.fillStyle = "#2f9aff";
+    context.fill();
+    context.stroke();
+
+    //rectangle
+    context.translate(200, 200);
+    context.beginPath();
+    context.rect(0, 0, 260*c, 130*c);
+    context.closePath();
+    context.strokeStyle = "#2f9aff";
+    context.fillStyle = "#2f9aff";
+    context.fill();
+    context.stroke();
+
 
     // axes
     context.beginPath();
@@ -81,10 +114,212 @@ function draw() {
     context.fillStyle = "black";
     context.stroke();
 
-
-    drawQuadrant("alpha", "b");
-    drawQuadrant("beta", "9");
-    drawQuadrant("gamma", "2");
-    drawQuadrant("delta", "6");
 }
 
+function submitClickCanvas(event) {
+    const rect = document.getElementById("axes").getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    transformPixels(x, y);
+}
+
+function transformPixels(x, y) {
+    c = 520*2/3;
+
+    x = (x-25)/c*2*rInput.value-rInput.value;
+    y = -((y-25)/c*2*rInput.value-rInput.value);
+
+    clickCanvas(x, y);
+}
+
+function clickCanvas(x, y) {
+    addPoint();
+}
+
+function drawPoint() {
+
+}
+
+function drawStep() {
+
+}
+
+beginTime();
+function beginTime() {
+    let container = document.getElementById("time-container");
+    changeTime(container);
+    setInterval(()=>changeTime(container), 1000)
+}
+function changeTime(container) {
+    let date = new Date();
+    let hours = date.getHours();
+    hours = hours<10 ? "0"+hours : hours;
+    let minutes = date.getMinutes();
+    minutes = minutes<10 ? "0"+minutes : minutes;
+    let seconds = date.getSeconds();
+    seconds = seconds<10 ? "0"+seconds : seconds;
+    container.innerHTML = `<p>Текущее время: ${hours}:${minutes}:${seconds}</p>`
+}
+
+/*
+
+function drawAxis() {
+    let h = canvas.height;
+    let w = canvas.width;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(w/2, h);
+    ctx.lineTo(w/2, 0);
+    ctx.lineTo(w/2+3, 7);
+    ctx.moveTo(w/2, 0);
+    ctx.lineTo(w/2-3, 7);
+    drawDigitsX(ctx, size, w, h);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0, h/2);
+    ctx.lineTo(w, h/2);
+    ctx.lineTo(w-7, h/2+3);
+    ctx.moveTo(w, h/2);
+    ctx.lineTo(w-7, h/2-3);
+    drawDigitsY(ctx, size, w, h);
+    ctx.stroke();
+    ctx.strokeStyle = "grey";
+    ctx.lineWidth = 1;
+    ctx.moveTo(w/2-5*size, h/2-5*size);
+    ctx.lineTo(w/2+3*size, h/2-5*size);
+    ctx.lineTo(w/2+3*size, h/2+5*size);
+    ctx.lineTo(w/2-5*size, h/2+5*size);
+    ctx.lineTo(w/2-5*size, h/2-5*size);
+    ctx.stroke();
+}
+function drawDigitsX(ctx, i, w, h) {
+    let t=w/2;
+    for (let j=0; j<5; j++){
+        t+=i;
+        ctx.moveTo(t, h/2+3);
+        ctx.lineTo(t, h/2-3)
+    }
+    t=w/2;
+    for (let j=0; j<5; j++){
+        t-=i;
+        ctx.moveTo(t, h/2+3);
+        ctx.lineTo(t, h/2-3)
+    }
+}
+function drawDigitsY(ctx, i, w, h) {
+    let t=h/2;
+    for (let j=0; j<5; j++){
+        t+=i;
+        ctx.moveTo(w/2+3, t);
+        ctx.lineTo(w/2-3, t);
+    }
+    t=h/2;
+    for (let j=0; j<5; j++){
+        t-=i;
+        ctx.moveTo(w/2+3, t);
+        ctx.lineTo(w/2-3, t);
+    }
+}
+function drawArea(r) {
+    let h = canvas.height;
+    let w = canvas.width;
+    ctx.strokeStyle = "#007765";
+    ctx.fillStyle = "#007765";
+    ctx.beginPath();
+    ctx.arc(w/2,h/2,r*size,0,Math.PI/2*3,true);
+    ctx.moveTo(w/2, h/2-r*size);
+    ctx.lineTo(w/2-r/2*size, h/2);
+    ctx.lineTo(w/2, h/2);
+    ctx.lineTo(w/2, h/2+r/2*size);
+    ctx.lineTo(w/2+r*size, h/2+r/2*size);
+    ctx.lineTo(w/2+r*size, h/2);
+    ctx.fill();
+}
+function drawPoint(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(canvas.width/2+x*size,canvas.height/2-y*size,2,0,Math.PI*2, true);
+    ctx.fill();
+}
+
+function clearCanvas() {
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawPointsFromTable() {
+    let table = document.getElementById("table-form:result-table").childNodes[3];
+    for(let row of table.children){
+        let x = Number(row.children[0].innerText);
+        let y = Number(row.children[1].innerText);
+        let ch = row.children[3].innerText;
+        drawPoint(x, y, isPointInArea(x, y, radius) ? "lime":"red")
+    }
+}
+function drawResize() {
+    table = document.getElementById("table-form:result-table").childNodes[3];
+    counter = 0;
+    drawStep();
+}
+
+function drawStep() {
+    console.log(counter);
+    if (counter < table.children.length){
+        let row = table.children[counter];
+        if(!row.children[0].innerText) return;
+        let x = Number(row.children[0].innerText);
+        let y = Number(row.children[1].innerText);
+        xCanvas.value = x;
+        yCanvas.value = y;
+        counter++;
+        checkResize();
+    }
+}
+
+ */
+
+/*
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let size = 25;
+let radius = 3;
+let xCanvas = document.getElementById("canvas-form:xCanvas");
+let yCanvas = document.getElementById("canvas-form:yCanvas");
+let counter;
+let table = document.getElementById("table-form:result-table").childNodes[3];
+drawArea(radius);
+drawAxis();
+drawResize();
+
+function handleRChange(event) {
+    clearCanvas();
+    radius = Number(event.target.value);
+    drawArea(radius);
+    drawAxis();
+    setTimeout(()=>drawResize(), 10);
+}
+
+function handleSubmit() {
+    clearCanvas();
+    drawArea(radius);
+    drawAxis();
+    drawResize();
+}
+
+function isPointInArea(x,y,r) {
+    return ((x<=0 && y>=0 && y<=(2*x+r)) || (x>=0 && y>=0 && x*x+y*y<=r*r) || (x>=0 && y<=0 && x<=r && y>=-r/2));
+}
+
+function handleCanvasClick(event) {
+    let obj = event.target;
+    let x = Number(((event.pageX - window.pageXOffset - obj.getBoundingClientRect().x - obj.width/2)/size).toFixed(2));
+    let y = Number((-(event.pageY - window.pageYOffset - obj.getBoundingClientRect().y - obj.height/2)/size).toFixed(2));
+    if(x>=-5 && x<=3 && y>=-5 && y<=5){
+        xCanvas.value = x;
+        yCanvas.value = y;
+        checkCanvas();
+    }
+}
+
+ */
